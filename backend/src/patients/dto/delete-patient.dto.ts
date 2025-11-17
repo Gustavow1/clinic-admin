4e8c89823f9a76ef35818d89596b5505d4b44424
Gustavow <114244232+Gustavow1@users.noreply.github.com>
2025-11-17 12:21:59 -0300

    updated delete-patient dto
    added description and example to swagger

--- backend/src/patients/dto/delete-patient.dto.ts
+++ backend/src/patients/dto/delete-patient.dto.ts
@@ -1,6 +1,11 @@
 import { IsNotEmpty, IsString } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
 
 export class DeletePatientDTO {
+  @ApiProperty({
+    description: "patient id",
+    example: "896449d9-ece7-40fc-9a92-c44f2143e3cf",
+  })
   @IsNotEmpty({ message: "id is required" })
   @IsString({ message: "id must be a string" })
   id: string;
